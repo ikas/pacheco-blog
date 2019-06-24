405f2d32940cc104e38ab11fd794461c3162e05f
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-06-24 20:56:45 +0100

    refact: remove unused components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
-import About from '../components/About'
 import Layout from '../components/Layout'
-import Timeline from '../components/Timeline'
 
 const IndexPage = () => (
   <Layout>
-    <Timeline />
-    <About />
+    <p>home page content</p>
   </Layout>
 )
 

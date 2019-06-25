61057e202211486493123ef7395a0677c22d3496
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-06-25 01:31:19 +0100

    feat: add placeholder content for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import Frame from '../components/full-screen-frame'
 import Header from '../components/ui-header'
+import Copy from '../components/copy'
+
+const ScreenWrapper = styled(Frame)`
+  display: flex;
+  flex-direction: column;
+`
+  
+const HomeFeature = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const ScrollText = styled(Copy)`
+  text-align: center;
+`
 
 export default () => (
   <Layout>
-    <Frame>
+    <ScreenWrapper>
       <Header currentUrl="/" />
-      <p>home page content</p>
-    </Frame>
+      <HomeFeature>
+        <Copy>Henrique Pacheco</Copy>
+        <Copy>Software Engineer from Portugal</Copy>
+      </HomeFeature>
+      <ScrollText>Scroll</ScrollText>
+    </ScreenWrapper>
   </Layout>
 )

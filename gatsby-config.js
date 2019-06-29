6a0e34c286f9bf49c61e437aceff13df2fdc6fe3
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-06-29 17:35:46 +0100

    feat: add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,11 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-85742872-1",
+      },
+    },
   ],
 }
